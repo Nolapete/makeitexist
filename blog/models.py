337285b0652bb0625bdf5b68d6daf005c500ce2d
Pete LoGiudice <pete.logiudice@zohomail.com>
2025-11-05 08:14:13 -0600

    Add blog initial post fixtures and categories plural name

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
 
